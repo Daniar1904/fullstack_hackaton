4a249c5807477cd4178a4fd92947db086ad1dd99
Syimykurmanov <syimyk.kurmanov@gmail.com>
2023-02-11 18:34:16 +0600

    added almost nothing

--- account/send_mail.py
+++ account/send_mail.py
@@ -3,10 +3,10 @@ from django.core.mail import send_mail
 
 def send_confirmation_email(user, code):
     full_link = f'http://localhost:8000/api/v1/accounts/activate/{code}/'
-    full_link_server = f'http://35.188.106.94:8000/api/v1/accounts/activate/{code}/'
+    # full_link_server = f'http://35.188.106.94:8000/api/v1/accounts/activate/{code}/'
     send_mail(
         'Здравствуйте, активируйте ваш аккаунт!',
-        f'Чтобы активировать ваш аккаунт нужно перейти по ссылке: \n{full_link}\n{full_link_server}',
+        f'Чтобы активировать ваш аккаунт нужно перейти по ссылке: \n{full_link}',
         'panda.takumi@gmail.com',
         [user],
         fail_silently=False
