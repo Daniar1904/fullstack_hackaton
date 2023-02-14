813a6817e5ed6c7bbb5eaa2ed96cf0f68196e375
Syimykurmanov <syimyk.kurmanov@gmail.com>
2023-02-14 22:16:13 +0600

    added settings

--- iStore/settings.py
+++ iStore/settings.py
@@ -168,7 +168,6 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = config('EMAIL_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_PASSWORD')
 
-
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
